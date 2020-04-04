23e91798a736a1109343025e66e92a228cc4fc60
luggaesch <lukas.schauhuber1@gmail.com>
2020-04-04 12:31:51 +0200

    Initialer Commit mit Aufgabenbeschreibung und Starterpaket

--- app/src/main/java/de/ur/mi/android/base/AlarmSystem.java
+++ app/src/main/java/de/ur/mi/android/base/AlarmSystem.java
@@ -6,45 +6,32 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 
 public class AlarmSystem {
-
     private final static int MOTION_DETECTION_Z = 15;
     private final static int Z_AXIS = 2;
+
     private final Context context;
     private final SensorEventListener listener;
     private SensorManager manager;
     private Sensor sensor;
 
     public AlarmSystem(Context context, SensorEventListener listener) {
-        this.context = context;
-        this.listener = listener;
-        init();
+        this.context = null;
+        this.listener = null;
     }
 
-    // Erstellt den SensorManager
-    // Erstellt den Sensor
     private void init() {
-        manager = (SensorManager) context
-                .getSystemService(Context.SENSOR_SERVICE);
-        sensor = manager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
     }
 
-    // Hängt den Listener aus der MainActivity an den Manager
     public void start() {
-        manager.registerListener(listener, sensor,
-                SensorManager.SENSOR_DELAY_UI);
+
     }
 
-    // Löst den Listener vom Manager
     public void stop() {
-        manager.unregisterListener(listener);
+
     }
 
-    // überprüft, ob der Schwellwert erreicht wird
     public boolean hasBeenMoved(final float[] values) {
-
-        if (values[Z_AXIS] > MOTION_DETECTION_Z) {
-            return true;
-        }
         return false;
     }
 }
--- app/src/main/java/de/ur/mi/android/base/MainActivity.java
+++ app/src/main/java/de/ur/mi/android/base/MainActivity.java
@@ -21,7 +21,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener, SensorEventListener {
 
     private final static String CLEAR = "";
-    private final static int RESET_INPUT_DELAY = 1000;
+    private final static int DELAY = 1000;
     private final static int COUNTDOWN = 4;
     private final static int PASSWORD_LENGTH = 4;
 
@@ -41,19 +41,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void initAlarm() {
-        alarm = new AlarmSystem(this, this);
+
     }
 
     private void initInput() {
-        input = findViewById(R.id.input_numbers);
+        input = (EditText) findViewById(R.id.input_numbers);
     }
 
-
-    // Initialisiert alle Buttons
     private void initNumpad() {
-        TableLayout table = findViewById(R.id.alarm_table);
-        Typeface face = Typeface.createFromAsset(getAssets(),
-                "fonts/led_real.ttf");
+        TableLayout table = (TableLayout) findViewById(R.id.alarm_table);
         int count = 0;
 
         for (int i = 0; i < table.getChildCount(); i++) {
@@ -62,7 +58,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 if (row.getChildAt(j) instanceof Button) {
                     Button button = (Button) row.getChildAt(j);
                     button.setText(String.valueOf(count + 1));
-                    button.setTypeface(face);
                     button.setOnClickListener(this);
                     count++;
                 }
@@ -77,96 +72,27 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void handleButtonClick(View v) {
-        // Wenn der Alarm läuft, wird dieser Teil des Codes ausgeführt
-        // Nach jedem Button Click wird die Länge des daraus resultierenden Stringd mit der Passwortlänge verglichen
-        // Wenn isCorrectPasswortd true liefert, wird der Alarm beendet
+        // if Alarm is still playing
         if (mediaPlayer != null && mediaPlayer.isPlaying()) {
-            String currentInput = setNewInput(v);
-            if (currentInput.length() == PASSWORD_LENGTH) {
-                if (isCorrectPassword()) {
-                    stopAlarm();
-                    clearPassword();
-                }
-                clearInput();
-            }
+
         } else if(!isRunning) {
-            // wenn kein Alarm läuft, wird dieser Teil des Codes ausgeführt
-            // Nach jedem Button Click wird die Länge des daraus resultierenden Strings mit der Passwortlänge verglichen
-            // Ist die Passwortlänge erreich,t wird das Passwort gespeichert und der Alarm gestartet
-            String currentInput = setNewInput(v);
-            if (currentInput.length() == PASSWORD_LENGTH) {
-                showCountdown();
-                saveInput();
-                clearInput();
-            }
+            // if no alarm is currently playing
         }
     }
 
-    // Vergleicht das gespeicherte Passwort mit dem Input
-    private boolean isCorrectPassword() {
-        return input.getText().toString().equals(password);
-    }
-
-    // Liest aus, welcher Button geklickt wurde und speichert den Wert in das Input Textview
     private String setNewInput(View v) {
-        String clickedNumber = (String) ((TextView) v).getText();
-        String oldInput = input.getText().toString();
-        input.setText(oldInput + clickedNumber);
-        return oldInput + clickedNumber;
+        return new String();
     }
 
-    // Hängt den Listener an das Alarmsystem oder entfernt ihn
-    private void setPhoneOnAlarm(boolean on) {
-        if (on) {
-            alarm.start();
-        } else {
-            alarm.stop();
-        }
-
-    }
-
-    // Erstellt einen neuen CountDownTimer
-    // Bei jedem Tick wird die Zeit per Toast dargestellt bis der Alarm aktiviert wurde
-    // Bei onFinish wird AlarmSystem gestartet, um darauf zu lauschen ob sich das Handy bewegt
     private void showCountdown() {
-        isRunning = true;
-        CountDownTimer countDown = new CountDownTimer(COUNTDOWN * RESET_INPUT_DELAY, 1000) {
 
-            @Override
-            public void onTick(long millisUntilFinished) {
-                Toast.makeText(getApplicationContext(), getString(R.string.toast_countdown) + (int) (millisUntilFinished / 1000), Toast.LENGTH_SHORT).show();
-
-            }
-
-            @Override
-            public void onFinish() {
-                setPhoneOnAlarm(true);
-            }
-        };
-
-        countDown.start();
 
     }
 
-    // Überschriebene Methode, die ausgeführt wird, wenn die Sensoren Änderungen feststellen
-    @Override
-    public void onSensorChanged(SensorEvent event) {
-        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-            if (alarm.hasBeenMoved(event.values.clone())) {
-                startAlarm();
-                setPhoneOnAlarm(false);
-            }
-
-        }
-
-    }
-
-    // Speichert das Passwort aus dem TextView in den String
     private void saveInput() {
-        password = input.getText().toString();
+
     }
 
-    // Setzt den TextView zurück
     private void clearInput() {
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
@@ -175,28 +101,37 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             public void run() {
                 input.setText(CLEAR);
             }
-        }, RESET_INPUT_DELAY);
+        }, DELAY);
 
     }
 
     private void clearPassword() {
-        password = null;
+
     }
 
-    // Alarm Sound wird gestartet
+    private boolean isCorrectPassword() {
+        return false;
+    }
+
+    private void setPhoneOnAlarm(boolean on) {
+
+    }
+
     private void startAlarm() {
         Toast.makeText(getApplicationContext(), "Alarm start", Toast.LENGTH_SHORT).show();
-        mediaPlayer = MediaPlayer.create(this, R.raw.alarm);
-        mediaPlayer.start();
-        mediaPlayer.setLooping(true);
+
     }
 
-    // MediaPlayer wird wieder gestoppt
     private void stopAlarm() {
         Toast.makeText(getApplicationContext(), "Alarm stopp", Toast.LENGTH_SHORT).show();
-        mediaPlayer.stop();
-        mediaPlayer.reset();
-        isRunning = false;
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+
+        }
     }
 
     @Override
